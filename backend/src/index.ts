040fea2bcbb12dc896239a4f16f94f12eda447f1
Joshua Ozorji <ozorjijoshua@gmail.com>
2025-11-04 07:39:42 +0100

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import express from "express";
+import connectToDatabase from "./config/db";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import { OK } from "./constants/http";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+	cors({
+		origin: APP_ORIGIN,
+		credentials: true,
+	}),
+);
+app.use(cookieParser());
+
+app.get("/", (req, res, next) => {
+	return res.status(OK).json({
+		status: "healthy",
+	});
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+	console.log(
+		`Server is running on port ${PORT} in ${NODE_ENV} environment`,
+	);
+	await connectToDatabase();
+});
