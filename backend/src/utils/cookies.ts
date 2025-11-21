4cd98fe56fc6bb368d48edb727970b897982b870
Joshua Ozorji <ozorjijoshua@gmail.com>
2025-11-21 12:16:15 +0100

    cookies

--- backend/src/utils/cookies.ts
+++ backend/src/utils/cookies.ts
@@ -4,7 +4,7 @@ import { fifteenMinutesFromNow, thirtyDaysFromNow } from "./date";
 export const REFRESH_PATH = "/auth/refresh";
 
 const defaults: CookieOptions = {
-	sameSite: "strict",
+	sameSite: "none",
 	httpOnly: true,
 	secure: true,
 };
